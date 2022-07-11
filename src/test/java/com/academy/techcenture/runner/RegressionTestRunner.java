72daae5c7a8f8fed3defa73d3a495c0dcb350c9e
anettka166 <anna_radionova@yahoo.com>
2022-07-11 17:34:16 -0400

    First day commits-eddited+

---
+++ src/test/java/com/academy/techcenture/runner/RegressionTestRunner.java
@@ -0,0 +1,16 @@
+package com.academy.techcenture.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        features = "src/tes/java/com/academy/techcenture/features",
+        glue = {"com/academy/techcenture/step_definitions"}
+)
+
+public class RegressionTestRunner {
+
+}
--- src/test/java/com/academy/techcenture/step_definitions/GoogleSearchStepDefinitions.java
+++ src/test/java/com/academy/techcenture/step_definitions/GoogleSearchStepDefinitions.java
@@ -1,5 +1,8 @@
 package com.academy.techcenture.step_definitions;
 
+import com.academy.techcenture.config.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,31 +15,36 @@ public class GoogleSearchStepDefinitions {
 
     private WebDriver driver;
 
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = Driver.getDriver();
+    }
+
     @Given("user goes to google search page")
     public void user_goes_to_google_search_page() {
-
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
         driver.get("https://www.google.com");
         String googleTitle = driver.getTitle();
         Assert.assertEquals("Titles don't match", "Google", googleTitle);
-
     }
 
     @When("user enters {string} in the search input box")
     public void user_enters_in_the_search_input_box(String string) {
-
     }
 
     @When("user presses enter key")
     public void user_presses_enter_key() {
-
     }
 
     @Then("user shoud see results for searched item")
     public void user_shoud_see_results_for_searched_item() {
-
     }
 
+    @After
+    public void cleanUp() {
+        if (driver != null) {
+            driver.quit();
+        }
 
+    }
 }
